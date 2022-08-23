8f81b2eb6feb9be907aecde6c90da563d7f75957
ilya <zapaska96@gmail.com>
2022-08-23 22:03:24 +0500

    выбор валюты при нажатии

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CursorBlock from "./components/CursorBlock";
 
 function App() {
+  const [rates, setRates] = useState({});
+  const [fromCurrency, setFromCurrency] = useState("RUB");
+  const [toCurrency, setToCurrency] = useState("USD");
+
+  useEffect(() => {
+    fetch("https://cdn.cur.su/api/latest.json")
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.rates);
+        console.log(json.rates);
+      })
+      .catch((err) => {
+        alert("Ошибка в получении валют");
+        console.error(err);
+      });
+  }, []);
+
   return (
     <div className="flex h-screen items-center justify-center gap-14 bg-slate-100 text-xl ">
       <div className="flex max-w-full flex-col items-center justify-center gap-14 rounded-xl bg-white p-20 shadow-lg lg:flex-row">
-        <CursorBlock currency="RUB" />
-        <CursorBlock currency="USD" />
+        <CursorBlock
+          currency={fromCurrency}
+          onChangeCurrency={setFromCurrency}
+        />
+        <CursorBlock currency={toCurrency} onChangeCurrency={setToCurrency} />
       </div>
     </div>
   );
